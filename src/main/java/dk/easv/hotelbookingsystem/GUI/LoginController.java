30a0b06b8a17ad869bc57adc8e6da061cc9e324a
Svoldgaard <Svoldgaard96@gmail.com>
2025-03-03 11:06:37 +0100

    LoginController

--- src/main/java/dk/easv/hotelbookingsystem/GUI/LoginController.java
+++ src/main/java/dk/easv/hotelbookingsystem/GUI/LoginController.java
@@ -1,14 +1,22 @@
 package dk.easv.hotelbookingsystem.GUI;
 
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class LoginController {
     @FXML
-    private Label welcomeText;
+    private MFXPasswordField txtUsername;
+    @FXML
+    private MFXPasswordField txtPassword;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void btnSave(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnCancel(ActionEvent actionEvent) {
     }
 }
