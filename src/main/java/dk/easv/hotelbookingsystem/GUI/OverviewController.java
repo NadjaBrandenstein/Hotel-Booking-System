41170e0e5b5312b2513688e899f0b45aa085ab51
Svoldgaard <Svoldgaard96@gmail.com>
2025-03-03 11:10:20 +0100

    OverviewController

--- src/main/java/dk/easv/hotelbookingsystem/GUI/OverviewController.java
+++ src/main/java/dk/easv/hotelbookingsystem/GUI/OverviewController.java
@@ -1,4 +1,61 @@
 package dk.easv.hotelbookingsystem.GUI;
 
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.mfxcore.controls.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class OverviewController {
+
+
+    @FXML
+    private MFXTextField txtSearch;
+    @FXML
+    private Label lblUser;
+
+
+    // TableView customer
+    @FXML
+    private ScrollPane spCustomer;
+    @FXML
+    private TableView tblCustomer;
+    @FXML
+    private TableColumn colFName;
+    @FXML
+    private TableColumn colLName;
+    @FXML
+    private TableColumn colPhoneNo;
+    @FXML
+    private TableColumn colEmail;
+
+    // TableView Rooms
+    @FXML
+    private ScrollPane spRoom;
+    @FXML
+    private TableView tblRoom;
+    @FXML
+    private TableColumn colRoomNo;
+    @FXML
+    private TableColumn colRoomSize;
+    @FXML
+    private TableColumn colPrice;
+    @FXML
+    private TableColumn colAvailable;
+
+    @FXML
+    private void btnLogout(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnNew(ActionEvent actionEvent) {
+    }
+
+    public void btnEdit(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete(ActionEvent actionEvent) {
+    }
 }
